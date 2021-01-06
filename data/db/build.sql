79e98c925c542671eecadeaf1104ea7d3caaf53b
UrbanHangman <orodgers141@gmail.com>
2021-01-06 21:16:31 +0000

    .

--- data/db/build.sql
+++ data/db/build.sql
@@ -1,6 +1,4 @@
-CREATE TABLE IF NOT EXISTS exp (
-    UserID integer PRIMARY KEY,
-    XP integer DEFAULT 0,
-    Level integer DEFAULT 0,
-    XPLock text DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS rules (
+    RuleNo INTEGER PRIMARY KEY NOTNULL,
+    Rule TEXT,
 );
